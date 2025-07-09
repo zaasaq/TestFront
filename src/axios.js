306b55863699fb77beda6c5f61e98f447f7f4eb8
unknown <jackbera708@gmail.com>
2025-07-09 12:08:06 +0200

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://back-fafv.onrender.com/api",
+  baseURL: "https://testback-z1jb.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
